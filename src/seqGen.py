03618ffc7b9d1f638204dfd20e1451192c1ef6e7
Arman Ashkari <arman.azad98@gmail.com>
2021-11-24 23:59:08 +0600

    seq to txt file
    Signed-off-by: Arman Ashkari <arman.azad98@gmail.com>

--- src/seqGen.py
+++ src/seqGen.py
@@ -1,63 +1,23 @@
+import os
 from numpy import random
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.random.mtrand import randint
 import seaborn as sns
-<<<<<<< HEAD
 import math
 
-_prim_len = 100
-_padding = 10
-_mew = 10
-_sigma = 5
+random.seed(10)
+
+_prim_len = 100000
+_padding = _prim_len/100
+_mu = _prim_len/100
+_sigma = _mu/2
 _lambda = 1
-=======
-import os
->>>>>>> acf38f6f6aa30b25b1f7018ea7f8f82651cd53be
-
-
-class treeNode:
-    def __init__(self, seq):
-        self.seq = seq
-        self.left = None
-        self.right = None
-
-    def is_leaf(self):
-        if self.left or self.right:
-            return False
-        return True
-
-    def get_newic(self):
-        if self.is_leaf():
-            return self.seq
-        left_str = self.left.get_newic()
-        right_str = self.right.get_newic()
-        return '(' + left_str + ',' + right_str + ')'
-
-
-N1 = treeNode('a')
-N2 = treeNode('b')
-N3 = treeNode('c')
-N4 = treeNode('d')
-N5 = treeNode('e')
-N6 = treeNode('')
-N7 = treeNode('')
-N8 = treeNode('')
-N9 = treeNode('')
-N6.left = N4
-N6.right = N5
-N7.left = N3
-N7.right = N6
-N8.left = N2
-N8.right = N7
-N9.left = N1
-N9.right = N8
-
-print(N9.get_newic())
+_lambda_tree = 5
 
 
 def get_prim_seq():
-    str = 'A'*(_prim_len//4) + 'T'*(_prim_len//4) + 'G'*(_prim_len//4) + 'C'*(_prim_len//4)
+    str = 'A'*(_prim_len//4) + 'T'*(_prim_len//4) + \
+        'G'*(_prim_len//4) + 'C'*(_prim_len//4)
     l = list(str)
     random.shuffle(l)
     str = ''.join(l)
@@ -66,25 +26,29 @@ def get_prim_seq():
 
 def deletion(seq):
     # from where
-    f = random.randint(_padding, len(seq)-_padding)
+    f = random.randint(_padding, len(seq)-_padding-_mu)
     # how much
-    l = math.floor(random.normal(loc=_mew, scale=_sigma))
+    l = math.floor(random.normal(loc=_mu, scale=_sigma))
     while l <= 0 or f+l >= len(seq)-_padding:
-        l = math.floor(random.normal(loc=_mew, scale=_sigma))
+        l = math.floor(random.normal(loc=_mu, scale=_sigma))
+        print('Inside deletion')
     str = seq[:f] + seq[f+l:]
     return str
 
 
 def tendem_repeat(seq):
     # from where
-    f = random.randint(_padding, len(seq)-_padding)
+    f = random.randint(_padding, len(seq)-_padding-_mu)
     # how much
-    l = math.floor(random.normal(loc=_mew, scale=_sigma))
+    l = math.floor(random.normal(loc=_mu, scale=_sigma))
     while l <= 0 or f+l >= len(seq)-_padding:
-        l = math.floor(random.normal(loc=_mew, scale=_sigma))
+        l = math.floor(random.normal(loc=_mu, scale=_sigma))
+        print('Inside tendem')
     # how many
     m = random.poisson(lam=_lambda) + 1
-    rep = '(' + seq[f:f+l] + ')'
+
+    # rep = '(' + seq[f:f+l] + ')'
+    rep = seq[f:f+l]
     str = seq[:f+l] + rep*m + seq[f+l:]
 
     # seq = seq[:f] + '(' + seq[f:f+l] + ')' + seq[f+l:]
@@ -95,24 +59,27 @@ def tendem_repeat(seq):
 
 def duplication(seq):
     # from where
-    f = random.randint(_padding, len(seq)-_padding)
+    f = random.randint(_padding, len(seq)-_padding-_mu)
     # how much
-    l = math.floor(random.normal(loc=_mew, scale=_sigma))
+    l = math.floor(random.normal(loc=_mu, scale=_sigma))
     while l <= 0 or f+l >= len(seq)-_padding:
-        l = math.floor(random.normal(loc=_mew, scale=_sigma))
+        l = math.floor(random.normal(loc=_mu, scale=_sigma))
+        print('Inside duplication 1')
     # how many
     m = random.poisson(lam=_lambda) + 1
     # to where
     arr = []
     for i in range(m):
-        num = random.randint(_padding, len(seq)-_padding)
-        while num >= f and num < f+l:
-            num = random.randint(_padding, len(seq)-_padding)
-        arr.append(num)
+        pos = random.randint(0, len(seq))
+        while pos >= f and pos < f+l:  # so that duplication is not placed within duplication
+            pos = random.randint(0, len(seq))
+            print('Inside duplication 2')
+        arr.append(pos)
     arr.sort()
 
     str = seq[:]
-    rep = '(' + seq[f:f+l] + ')'
+    # rep = '(' + seq[f:f+l] + ')'
+    rep = seq[f:f+l]
     for i in range(len(arr)):
         str = str[:arr[i]+i*l] + rep + str[arr[i]+i*l:]
 
@@ -122,23 +89,77 @@ def duplication(seq):
     return str
 
 
-str = get_prim_seq()
-print(str)
+def mutation(seq):
+    # print("mutation on %s" % (seq))
+    new_seq = seq
+    m = random.poisson(lam=_lambda_tree)
+    arr = random.randint(1, 3, size=m)
+    for a in arr:
+        if a == 1:
+            new_seq = deletion(new_seq)
+        elif a == 2:
+            new_seq = tendem_repeat(new_seq)
+        else:
+            new_seq = duplication(new_seq)
+    return new_seq
 
-<<<<<<< HEAD
-tendem_repeat(str)
-duplication(str)
-=======
 
-def write_to_fasta(seq):
-    path = os.path.abspath("../data/ref.txt")
+all_nodes = {}
+
+
+class treeNode:
+    def __init__(self, seq, label):
+        global all_nodes
+        self.seq = seq
+        self.label = label
+        self.left = None
+        self.right = None
+        all_nodes[label] = seq
+
+    def is_leaf(self):
+        if self.left or self.right:
+            return False
+        return True
+
+    def get_newic(self):
+        if self.is_leaf():
+            return self.label
+        left_str = self.left.get_newic()
+        right_str = self.right.get_newic()
+        return '(' + left_str + ',' + right_str + ')'
+
+
+seq = get_prim_seq()
+N1 = treeNode(mutation(seq), 'abcdefghij')
+N2 = treeNode(mutation(N1.seq), 'abcde')
+N3 = treeNode(mutation(N2.seq), 'ab')
+N4 = treeNode(mutation(N3.seq), 'a')
+N5 = treeNode(mutation(N3.seq), 'b')
+N6 = treeNode(mutation(N2.seq), 'cde')
+N7 = treeNode(mutation(N6.seq), 'c')
+N8 = treeNode(mutation(N6.seq), 'de')
+N9 = treeNode(mutation(N8.seq), 'd')
+N10 = treeNode(mutation(N8.seq), 'e')
+N11 = treeNode(mutation(N1.seq), 'fghij')
+N12 = treeNode(mutation(N11.seq), 'fg')
+N13 = treeNode(mutation(N12.seq), 'f')
+N14 = treeNode(mutation(N12.seq), 'g')
+N15 = treeNode(mutation(N11.seq), 'hij')
+N16 = treeNode(mutation(N15.seq), 'h')
+N17 = treeNode(mutation(N15.seq), 'ij')
+N18 = treeNode(mutation(N17.seq), 'i')
+N19 = treeNode(mutation(N17.seq), 'j')
+
+
+def write_to_fasta(label, seq):
+    path = os.path.abspath("..\copyEvol\data\%s.txt" % (label))
     outfile = open(path, "w")
-    outfile.write(">" + "primary_seq" + "\n" + seq + "\n")
+    outfile.write(">" + label + "\n" + seq + "\n")
     outfile.close()
 
 
 def write_to_newick(root):
-    path = os.path.abspath("../data/ref_tree.newick")
+    path = os.path.abspath("..\copyEvol\data\ref_tree.newick")
     outfile = open(path, "w")
     outstr = root.get_newic()
     outstr = "[&R] " + outstr + ";"
@@ -146,6 +167,8 @@ def write_to_newick(root):
     outfile.close()
 
 
-write_to_fasta(get_prim_seq())
-write_to_newick(N9)
->>>>>>> acf38f6f6aa30b25b1f7018ea7f8f82651cd53be
+for label in all_nodes:
+    write_to_fasta(label, all_nodes[label])
+
+# write_to_fasta(get_prim_seq())
+# write_to_newick(N1)
